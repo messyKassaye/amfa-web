7692d375de17ffea1dec59e8e10405aca3dbf32a
Meseret <meseret.kassayes@gmail.com>
2021-12-03 14:25:25 +0300

    nav added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AuthDashboard from "../auth/AuthDashboard";
 import Home from "../home/Home";
 import AuthenticatedRoute from "./AuthenticationRoute";
@@ -9,7 +9,8 @@ export default function Routes() {
     <Router>
       <Switch>
         <AuthenticatedRoute path={"/auth"} component={AuthDashboard} />
-        <PrivateRoute path="/" component={Home} />
+        <PrivateRoute path="/" component={AuthDashboard} />
+        <Route path={"/home"} component={AuthDashboard} />
       </Switch>
     </Router>
   );
--- src/auth/AuthDashboard.js
+++ src/auth/AuthDashboard.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { Layout } from "antd";
+import "antd/dist/antd.less";
 
 export default function AuthDashboard() {
-  return <div>Auth AuthDashboard</div>;
+  const { Header, Sider, Content, Footer } = Layout;
+  return (
+    <Layout>
+      <Sider>
+        <div
+          className="flex h-fullf"
+          style={{ height: "100vh", background: "red" }}
+        ></div>
+      </Sider>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>
+  );
 }
